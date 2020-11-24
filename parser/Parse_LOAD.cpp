23b0a2a772fb667fcef5a0653a52186e6d72d498
Doris404 <kk15910306736@163.com>
2020-11-24 08:38:24 +0800

    UPDATE Parse_LOAD.cpp

--- parser/Parse_LOAD.cpp
+++ parser/Parse_LOAD.cpp
@@ -6,45 +6,59 @@
 #include "../metadata/metadata.h"
 
 using namespace std;
-
+// local create
 int GetSite(string sql_statement);
-string GetFragType(string sql_statement);
-string GetCreateSql(string sql_statement);
-string GetSqlFromGddCols(vector<ColumnDef> cols);
-string GetLoadSql(string sql_statement);
-string GetLocalFile(string sql_statement);
+string GetLocalCreate(string sql_statement);
+// local load ALLDATA
+string GetPathFromLocalLoad(string sql_statement);
+string GetTableFromLocalLoad(string sql_statement);
+// local select and load
+string GetSelectSqlFromLoadLocalData(string sql_statement);
+string GetToTable(string sql_statement);
+// string GetFragType(string sql_statement);
+// string GetCreateSql(string sql_statement);
+// string GetSqlFromGddCols(vector<ColumnDef> cols);
+// string GetLoadSql(string sql_statement);
+// string GetLocalFile(string sql_statement);
+
 
 int main() {
+    // H
     string sql_statement;
-    // sql_statement = "CREATE TABLE Publisher (id int(6) key, name char(100), nation char(3)) ON SITE 1;";
-    sql_statement = "LOAD ALLDATA TO TABLE Publisher FROM LOCAL FILE the path;";
-    // sql_statement = "LOAD LOCALDATA TABLE Publisher BY H WITH ( id<104000 nation='PRC') TO Publiser.1 ON SITE 1;";
+    sql_statement = "LOCAL CREATE TABLE Publisher (id int(6) key, name char(100), nation char(3)) ON SITE 1;";
+    int site = GetSite(sql_statement);
+    string sql_localcreate = GetLocalCreate(sql_statement);
+    cout << site << endl;
+    cout << sql_localcreate << endl;
+    sql_statement = "LOCAL LOAD ALLDATA TO TABLE Publisher FROM LOCAL FILE the path;";
+    string table_name = GetTableFromLocalLoad(sql_statement);
+    string path = GetPathFromLocalLoad(sql_statement);
+    cout << table_name << endl;
+    cout << path << endl;
+    sql_statement = "LOCAL LOAD LOCALDATA TABLE Publisher BY H WITH ( id<104000 nation='PRC') TO Publiser.1 ON SITE 1;";
+    string select_sql;
+    select_sql = GetSelectSqlFromLoadLocalData(sql_statement);
+    string to_table; 
+    to_table = GetToTable(sql_statement);
+    cout << to_table << endl;
     
+    // V
+    // sql_statement = "LOCAL CREATE TABLE Customer (id int(6) key, name char(100), rank int(6)) ON SITE 1;";
     // int site = GetSite(sql_statement);
-    // string type = GetFragType(sql_statement);
-    // cout <<type.size()<<endl;
-    // string sql_statement_create = GetCreateSql(sql_statement);
-    // string sql_statement_load = GetLoadSql(sql_statement);
-    string local_file = GetLocalFile(sql_statement);
-
+    // string sql_localcreate = GetLocalCreate(sql_statement);
     // cout << site << endl;
-    // cout << type << endl;
-    // cout << sql_statement_create << endl;
-    // cout << sql_statement_load << endl;
-    cout << local_file << endl;
-
-
-    // sql_statement = "LOAD TABLE Customer TO SITE 1 BY V WITH ( id rank) AS Customer.1 THE LOCAL FILE IS the path;";
-    // int site = GetSite(sql_statement);
-    // string type = GetFragType(sql_statement);
-    // string sql_statement_create = GetCreateSql(sql_statement);
-    // string sql_statement_load = GetLoadSql(sql_statement);
-
-    // cout << site << endl;
-    // cout << type << endl;
-    // cout << sql_statement_create << endl;
-    // cout << sql_statement_load << endl;
-
+    // cout << sql_localcreate << endl;
+    // sql_statement = "LOCAL LOAD ALLDATA TO TABLE Customer FROM LOCAL FILE the path;";
+    // string table_name = GetTableFromLocalLoad(sql_statement);
+    // string path = GetPathFromLocalLoad(sql_statement);
+    // cout << table_name << endl;
+    // cout << path << endl;
+    // sql_statement = "LOCAL LOAD LOCALDATA TABLE Customer BY V WITH ( id name) TO Customer.1 ON SITE 1;";
+    // string select_sql;
+    // select_sql = GetSelectSqlFromLoadLocalData(sql_statement);
+    // string to_table; 
+    // to_table = GetToTable(sql_statement);
+    // cout << to_table << endl;
     return 0;
 }
 
@@ -106,4 +120,45 @@ string GetLocalFile(string sql_statement) {
     string local_file;
     local_file = GetBetween(sql_statement,"FROM LOCAL FILE",";");
     return local_file;
+}
+string GetLocalCreate(string sql_statement) {
+    string localcreate;
+    localcreate = GetBetween(sql_statement,"LOCAL","ON") + ";";
+    return localcreate;
+}
+string GetTableFromLocalLoad(string sql_statement) {
+    return GetBetween(sql_statement,"TABLE","FROM");
+}
+string GetPathFromLocalLoad(string sql_statement) {
+    return GetBetween(sql_statement, "FILE",";");
+}
+
+string GetSelectSqlFromLoadLocalData(string sql_statement) {
+    string frag_type = GetFragType(sql_statement);
+    string to_table = GetBetween(sql_statement, "TO", "ON");
+    string from_table = GetBetween(sql_statement, "TABLE", "BY");
+    string select_sql;
+    if (frag_type == "H") {
+        string condition = GetBetween(sql_statement, "(",")");
+        vector<string> condition_list = GetList(condition," ",")");
+        condition = Link(condition_list, " AND ");
+        select_sql = "SELECT * FROM " + from_table + " WHERE " + condition + " ;";
+    }
+    else if (frag_type == "V") {
+        string condition = GetBetween(sql_statement, "(",")");
+        vector<string> condition_list = GetList(condition," ",")");
+        condition = Link(condition_list, ", ");
+        select_sql = "SELECT " + condition + " FROM " + from_table; 
+    }
+    else {
+        cout << "INVALID FRAGMENT TYPE " << endl;
+        return "ERROR";
+    }
+    cout << select_sql << endl;
+    return select_sql;
+}
+
+string GetToTable(string sql_statement) {
+    string to_table = GetBetween(sql_statement, "TO", "ON");
+    return to_table;
 }

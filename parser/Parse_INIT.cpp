72de7055c41546f8ad043435d507737e508d9cfa
Doris404 <kk15910306736@163.com>
2020-12-07 14:57:29 +0800

    update

--- parser/Parse_INIT.cpp
+++ parser/Parse_INIT.cpp
@@ -1,24 +1,6 @@
-#include "iostream"
-#include <stdio.h>
-#include <vector>
-#include <string>
-#include"Tools.h"
-#include "../metadata/metadata.h"
+#include "Parse_INIT.h"
 
 using namespace std;
-// Create table
-string InitGetTableCreateTable(string sql_statement);
-vector<ColumnDef> InitGetColumnsCreateTable(string sql_statement);
-GDD InitGetGDDCreateTable(string sql_statement);
-void TraverseGDD(GDD gdd);
-// Create Fragment
-string InitGetTableCreateFragmentation(string sql_statement);
-string InitGetFragmentTypeCreateFragmentation(string sql_statement);
-vector<FragDef> InitGetFragDefCreateFragmentation(string sql_statement);
-int InitGetFragNumCreateFragmentation(string sql_statement);
-void Traversefrags(vector<FragDef> frags);
-void TraverseFragment(Fragment);
-Fragment InitGetFragmentCreateFragment(string sql_statement);
 
 // int main() {
 //     // Create table
